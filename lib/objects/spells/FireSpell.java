84ac2695a0445bce7bf4c06e35df20392d4968f7
CJ-Uy <75460325+CJ-Uy@users.noreply.github.com>
2025-04-17 15:28:01 +0800

    reinitialized spell classes

---
+++ lib/objects/spells/FireSpell.java
@@ -0,0 +1,17 @@
+package lib.objects.spells;
+
+import java.awt.Graphics2D;
+import lib.objects.*;
+
+class FireSpell extends GameObject {
+
+    public FireSpell(double x, double y, double width, double height) {
+        super(x, y, width, height);
+    }
+
+    @Override
+    public void drawSprite(Graphics2D g2d) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
